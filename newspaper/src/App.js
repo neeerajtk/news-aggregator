bf1d0dedce3ffeee1bdd52cb8e90e3909c32b6fe
neeerajtk <neerajtk560@gmail.com>
2020-06-01 20:49:09 +0530

    api added

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
+    const url =  "https://gnews.io/api/v3/{endpoint}?token=b9a8f27653efa822e6533b678916eb30";
+    // GET https://gnews.io/api/v3/search?q=example&token=API-Token 
+    // b9a8f27653efa822e6533b678916eb30
+
+
     return (
         <div className="App">
           <header className="App-header">
