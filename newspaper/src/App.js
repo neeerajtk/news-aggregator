a8278842e07f1069dd5e226ca67828053457b7c4
neeerajtk <neerajtk560@gmail.com>
2020-06-01 20:13:13 +0530

    base files

---
+++ newspaper/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>NEWSPAPER</h1>   
+        </div>
+    )
+}
+
+export default App
