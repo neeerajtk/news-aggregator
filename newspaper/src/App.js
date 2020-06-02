f8b58b2b71e3f5f7082f5a8824db16163f01a221
neeerajtk <neerajtk560@gmail.com>
2020-06-02 14:28:48 +0530

    weather api added

--- newspaper/src/App.js
+++ newspaper/src/App.js
@@ -13,6 +13,10 @@ const App = () => {
     const url =  `https://gnews.io/api/v3/search?q=${query}&token=${APP_KEY}`;
     // GET https://gnews.io/api/v3/search?q=example&token=API-Token 
     // b9a8f27653efa822e6533b678916eb30
+    
+    
+    const WAPP_KEY = "8a8f33585b25d8f61b26f7cfa645d3a5";
+    const wurl = `api.openweathermap.org/data/2.5/weather?q=${wquery}&appid=${WAPP_KEY}`
 
     const getData = async () => {
         const result = await Axios.get(url)
@@ -41,10 +45,22 @@ const App = () => {
         getData();
     };
 
+    
+
+
     return (
         <div className="App">
           <header className="App-header">
-             <h1> NEWSPAPER.IO</h1>  
+             <h1> NEWSPAPER.IO</h1>
+             <form className="weather-search-form" onSubmit={onSubmitWeather}>
+                 <input 
+                 type="text"
+                 placeholder="Search City!" 
+                 onChange={onChangeWeather}
+                 value={wquery}
+                 />
+                 <input type="submit" value="Find"/>
+            </form>  
            </header> 
            <form className="search-form" onSubmit={onSubmit}>
                  <input 
