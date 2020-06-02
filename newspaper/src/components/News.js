28a3732fa4a50dfcb1b0ea529d277573ca09ab2f
neeerajtk <neerajtk560@gmail.com>
2020-06-02 11:08:50 +0530

    material ui skelton cards added

--- newspaper/src/components/News.js
+++ newspaper/src/components/News.js
@@ -1,16 +1,100 @@
-import React from 'react'
+// import React from 'react'
 
-const News = ({newsItem}) => {
-    const {title,description,image,url} = newsItem;
+// const News = ({newsItem}) => {
+//     const {title,description,image,url} = newsItem;
 
-    return (
-        <div className="news_disp">
-            <h2>{title}</h2>
-            <img src={image} alt={title}/>
-            <a href={url}>Read More</a>
-            {/* <button>Read More...</button> */}
-        </div>
-    )
+//     return (
+//         <div className="news_disp">
+//             <h2>{title}</h2>
+//             <img src={image} alt={title}/>
+//             <a href={url}>Read More</a>
+//             {/* <button>Read More...</button> */}
+//         </div>
+//     )
+// }
+
+// export default News
+
+
+
+
+// ******** 
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Skeleton from '@material-ui/lab/Skeleton';
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    maxWidth: 345,
+    margin: theme.spacing(2),
+  },
+  media: {
+    height: 190,
+  },
+}));
+
+function News(newsItem) {
+  const {title,description,image,url} = newsItem;
+
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+            <Avatar
+              alt="Ted talk"
+              src="https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg"
+            />
+        }
+        action={
+            <IconButton aria-label="settings">
+              <MoreVertIcon />
+            </IconButton>
+        }
+        title={
+            title
+        }
+        subheader={'5 hours ago'}
+      />
+      {
+        <CardMedia
+          className={classes.media}
+          image="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512"
+          title="Ted talk"
+        />
+      }
+
+      <CardContent>
+        {
+          <Typography variant="body2" color="textSecondary" component="p">
+            {
+              "Why First Minister of Scotland Nicola Sturgeon thinks GDP is the wrong measure of a country's success:"
+            }
+          </Typography>
+        }
+      </CardContent>
+    </Card>
+  );
 }
 
-export default News
+
+
+export default function Facebook() {
+  return (
+    <div>
+      <News />
+    </div>
+  );
+}
